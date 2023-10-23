b8efdcdeaf5cff14c2cb3161cd2f225d5c35b7a8
tushar-1265 <agarwalsatih980@gmail.com>
2023-10-23 15:22:16 +0530

    removed animations

--- script.js
+++ script.js
@@ -55,33 +55,33 @@ gsap.to("#main", {
     }
 });
 
-// gsap.from("#about-us img,#about-us-in", {
-//     y: 90,
-//     opacity: 0,
-//     duration: 1,
-//     scrollTrigger: {
-//       trigger: "#about-us",
-//       scroller: "body",
-//       markers:true,
-//       start: "top 70%",
-//       end: "top 65%",
-//       scrub: 2,
-//     },
-//   });
-
-  gsap.from(".card", {
-    scale:0.8,
+gsap.from("#about-us img,#about-us-in", {
+    y: 90,
     opacity: 0,
     duration: 1,
-    stagger:0.1,
     scrollTrigger: {
-      trigger: ".card",
+      trigger: "#about-us",
       scroller: "body",
+      markers:false,
       start: "top 70%",
       end: "top 65%",
-      scrub: 1,
+      scrub: 2,
     },
   });
+
+  // gsap.from(".card", {
+  //   scale:0.8,
+  //   opacity: 0,
+  //   duration: 1,
+  //   stagger:0.1,
+  //   scrollTrigger: {
+  //     trigger: ".card",
+  //     scroller: "body",
+  //     start: "top 70%",
+  //     end: "top 65%",
+  //     scrub: 1,
+  //   },
+  // });
 
   gsap.from("#colon1", {
     y: -70,
